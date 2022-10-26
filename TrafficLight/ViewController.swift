927edb1442353f05eb93b40f115b70ec3a7ece96
Александр Биктеев <bikteev@mail.ru>
2022-10-26 12:04:15 +0600

    Setup UI Elements

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var redView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
